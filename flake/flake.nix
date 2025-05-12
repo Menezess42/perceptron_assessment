c0a0588fa4d6032e1e47da3cb66966982392b879
Menezess42 <arielmenezess@hotmail.com>
2025-05-12 19:30:35 -0300

    create teh gitIgnore

---
+++ flake/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Projeto que estende o ambiente Essentials";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    essentials.url = "git+file:///mnt/hdmenezess42/GitProjects/flakeEssentials";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, essentials }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        baseShell = essentials.devShells.${system}.python;
+      in {
+        devShell = pkgs.mkShell {
+          name = "projeto-com-requests";
+
+          buildInputs = baseShell.buildInputs ++ (with pkgs.python311Packages; [
+          # opencv4
+          ]);
+
+          shellHook = ''
+            echo "Ambiente do projeto carregado (base Essentials + customizações)."
+            ${baseShell.shellHook or ""}
+          '';
+        };
+      });
+}
