1b7b3729954cc0900b24f6893c5367ddae86db66
Menezess42 <arielmenezess@hotmail.com>
2025-05-14 16:01:01 -0300

    Create the save model and train function

--- Iris/Iris.py
+++ Iris/Iris.py
@@ -78,7 +78,22 @@ class PerceptronNetwork:
         self.bias = bias
 
         if weights == []:
-            self.weights = np.random.rand(numberOfInputs+1)
+            self.weights = np.random.rand(self.numberOfPerceptrons * (self.numberOfInputs + 1))
+
+    def save_model(self, file_path: str):
+        '''
+        salva o modelo em um arquvio JSON.
+        '''
+        model_data = {
+                "numberOfPerceptrons": self.numberOfPerceptrons,
+                "numberOfInputs": self.numberOfInputs,
+                "numberOfOutputs": self.numberOfOutputs,
+                "bias": self.bias,
+                "weights": self.weights.tolist()
+                }
+        with open(file_path, 'w') as f:
+            json.dump(model_data, f, indent=4)
+        print(f"Modelo salvo em {file_path}")
 
     def shuffle(self, data):
         np.random.shuffle(data)
@@ -130,6 +145,22 @@ class PerceptronNetwork:
         train = data[test_size+val_size:]
         return train, val, test
 
+    def softMax(self, z: np.ndarray) -> np.ndarray:
+        '''
+        Aplica a função softmax sobre o vetor de saídas
+        z dos perceptrons.
+        Z: vetor numpy com as ativações dos perceptrons
+        (shape: [numberOfPerceptrons])
+        Retorna: Vetor com as probabilidades normalizadas
+        (shape: [numberOfPerceptrons])
+        '''
+        z = z.astype(np.float64)
+        z_stable = z - np.max(z)
+        exp_z = np.exp(z_stable)
+        softmax = exp_z / np.sum(exp_z)
+        return softmax
+
+
     def main(self, file_path: str):
         '''
         file_path: Caminho dos dados
@@ -138,11 +169,79 @@ class PerceptronNetwork:
         data = self.from_raw_to_matrix(raw_data)
         hoted_data, unique_cats = self.one_hotEncoder(data)
         shuffled_data = self.shuffle(hoted_data)
-        print(len(shuffled_data))
         train, val, test = self.divid_data(perTrain=0.7, perTest=0.15, data=shuffled_data)
+        self.save_model('./Reports/model_b4_train.json')
+        self.train(train)
 
-        
+    def train(self,
+              train,
+              learning_rate=0.01,
+              epochs=100):
+        '''
+        Treina a rede usando gradiente descendente.
+        train: dados de treinamento (entrada + saída one-hot)
+        '''
+        train = np.array(train)
+        for epoch in range(epochs):
+            total_loss = 0
+            for row in train:
+                *x, y_true = row[:-self.numberOfPerceptrons], row[-self.numberOfPerceptrons:]
+                x = np.array(x, dtype=float)
+                y_true = np.array(y_true, dtype=int)
+                
+                # Forward
+                y_pred = self.forward(x)
 
+                # Perda (entropia cruzada)
+                loss = -np.sum(y_true*np.log(y_pred+1e-15))
+                total_loss += loss
+
+                # Gradiente da softmax com entropia cruzada
+                grad = y_pred - y_true # shape: [numberOfPerceptrons]
+
+                # prepara x com bias
+                x_bias = np.append(x, self.bias) # shape: [numberOfInputs + 1]
+
+                # Atualiza os pesos para cada perceptron
+                for i in range(self.numberOfPerceptrons):
+                    idx_start = i*(self.numberOfInputs+1)
+                    idx_end = idx_start + self.numberOfInputs+1
+                    self.weights[idx_start:idx_end] -= learning_rate * grad[i] * x_bias
+
+            # Epoch feddback
+            average_loss = total_loss/len(train)
+            print(f'Época {epoch+1}/{epochs} - perda total: {total_loss:.4f} - Perda média: {average_loss:.4f}')
+
+        # Salva o modelo após o treinamento
+        save_path = './Reports/model.json'
+        self.save_model(save_path)
+
+    
+    def test(self, test):
+        ...
+
+    def valid(self, valid):
+        ...
+    
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        '''
+        Calcula a saída da rede (logists)+ aplica softmax.
+        x: vetor de entrada (shape: [numberOfInputs])
+        retorna: vetor de probabilidades (shape: [numberOfPerceptrons])
+        '''
+        # Adiciona o bias como entrada extra
+        x = np.append(x, self.bias)
+
+        # Grante que os pesos estão reshaped corretamente
+        weights_matrix = self.weights.reshape(self.numberOfPerceptrons, self.numberOfInputs + 1)
+
+        # Produto escalar de cadad neurônio com os inputs
+        logits = np.dot(weights_matrix, x)
+
+        # Softmax para converter logits em probabilidades
+        returns = self.softMax(logits)
+        return returns
+    
 
 def main():
     pn = PerceptronNetwork(numberOfPerceptrons=3,
